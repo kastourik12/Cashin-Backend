3e608e48ab3d30a1374e243dac31ef17d6f7ca03
kastourik12 <kastouriw@yahoo.Fr>
2022-06-09 11:29:46 +0100

    first commit

---
+++ src/main/java/com/example/springmongodbjwt/SpringMongodbJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmongodbjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongodbJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springmongodbjwt/SpringMongodbJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmongodbjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
